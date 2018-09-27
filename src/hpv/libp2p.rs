200e8625065bf2e5ecf760009874fe9d9ee466cf
Merlijn Boogerd <mboogerd@xebia.com>
2018-09-27 08:43:27 +0200

    Compilation without boxing...

--- src/hpv/libp2p.rs
+++ src/hpv/libp2p.rs
@@ -3,13 +3,16 @@ use super::super::proto::hpv::*;
 use super::super::protobuf;
 use super::super::protobuf::Message;
 use super::super::unsigned_varint::codec;
-use super::bytes::Bytes;
+use super::actix::dev;
+use super::bytes::{Bytes, BytesMut};
 use super::futures::future;
 use super::futures::future::{Future, FutureResult};
 use super::futures::prelude::*;
+use super::futures::sink::{SinkMapErr, With};
 use super::futures::stream;
 use super::futures::stream::*; //{Forward, FromErr, MapErr, Repeat, SplitSink, SplitStream, Zip};
 use super::futures::sync::mpsc;
+use super::futures::*; //{Forward, FromErr, MapErr, Repeat, SplitSink, SplitStream, Zip};
 use super::libp2p_core::*;
 use super::tokio_codec::Framed;
 use super::tokio_io::{AsyncRead, AsyncWrite};
@@ -21,7 +24,6 @@ use std::iter;
 pub struct HpvConfig {
     self_peer: mpsc::Sender<HpvMsg>,
     my_addr_proto: proto::hpv::Peer,
-    recv_peer: mpsc::Receiver<Peer>,
     mock_temp_peer: Peer,
 }
 
@@ -37,7 +39,7 @@ where
         iter::once(("/demograph/hyparview/1.0.0".into(), ()))
     }
 
-    type Output = HpvFuture;
+    type Output = HpvFuture<C>;
     type MultiaddrFuture = Maf;
     type Future = FutureResult<(Self::Output, Self::MultiaddrFuture), IoError>;
 
@@ -54,11 +56,17 @@ where
 }
 
 #[must_use = "futures do nothing unless polled"]
-pub struct HpvFuture {
-    inner: Box<Future<Item = (), Error = IoError> + Send>,
+pub struct HpvFuture<S>
+where
+    S: AsyncRead + AsyncWrite + Send + 'static,
+{
+    inner: NormalizedBridge<S>,
 }
 
-impl Future for HpvFuture {
+impl<S> Future for HpvFuture<S>
+where
+    S: AsyncRead + AsyncWrite + Send + 'static,
+{
     type Item = ();
     type Error = IoError;
 
@@ -68,13 +76,66 @@ impl Future for HpvFuture {
     }
 }
 
-impl fmt::Debug for HpvFuture {
+//use super::futures::{Map, Select};
+//use super::futures::{Forward, AndThen, Zip, SplitStream, FromErr};
+//type HpvFutureInternal = Map<Select<Map<Forward<AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>>, fn((AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)) {hpv::libp2p::forget::<(AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)>}>, Map<Forward<MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>>, fn((MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)) {hpv::libp2p::forget::<(MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)>}>>, fn(((), SelectNext<Map<Forward<AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>>, fn((AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)) {hpv::libp2p::forget::<(AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)>}>, Map<Forward<MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>>, fn((MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)) {hpv::libp2p::forget::<(MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)>}>>)) {hpv::libp2p::forget::<((), SelectNext<Map<Forward<AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>>, fn((AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)) {hpv::libp2p::forget::<(AndThen<Zip<SplitStream<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, Repeat<Peer, IoError>>, fn((BytesMut, Peer)) -> Result<hpv::message::HpvMsg, IoError>, Result<hpv::message::HpvMsg, IoError>>, sink::SinkMapErr<hpv::libp2p::RecipientWrapper, fn(hpv::actix::prelude::SendError<hpv::message::HpvMsg>) -> IoError>)>}>, Map<Forward<MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>>, fn((MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)) {hpv::libp2p::forget::<(MapErr<Zip<sync::mpsc::Receiver<hpv::message::HpvMsg>, Repeat<proto::hpv::Peer, ()>>, fn(()) -> IoError>, sink::With<SplitSink<FromErr<Framed<S, codec::UviBytes<std::vec::Vec<u8>>>, IoError>>, (hpv::message::HpvMsg, proto::hpv::Peer), fn((hpv::message::HpvMsg, proto::hpv::Peer)) -> Result<std::vec::Vec<u8>, IoError>, Result<std::vec::Vec<u8>, IoError>>)>}>>)>}>
+
+impl<S> fmt::Debug for HpvFuture<S>
+where
+    S: AsyncRead + AsyncWrite + Send + 'static,
+{
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         fmt.debug_struct("HpvFuture").finish()
     }
 }
 
-fn hyparview_protocol<S>(socket: S, config: HpvConfig) -> HpvFuture
+type ProtoSink<S> = SplitSink<FromErr<Framed<S, codec::UviBytes<Vec<u8>>>, IoError>>;
+type ProtoSource<S> = SplitStream<FromErr<Framed<S, codec::UviBytes<Vec<u8>>>, IoError>>;
+type ProtoToHpv<S> = With<
+    ProtoSink<S>,
+    (HpvMsg, proto::hpv::Peer),
+    fn((HpvMsg, proto::hpv::Peer)) -> Result<Vec<u8>, IoError>,
+    Result<Vec<u8>, IoError>,
+>;
+type HpvToProto<S> = stream::AndThen<
+    Zip<ProtoSource<S>, stream::Repeat<Peer, IoError>>,
+    fn((BytesMut, Peer)) -> Result<HpvMsg, IoError>,
+    Result<HpvMsg, IoError>,
+>;
+type ZipReceiverPeer = stream::MapErr<
+    Zip<mpsc::Receiver<HpvMsg>, stream::Repeat<proto::hpv::Peer, ()>>,
+    fn(()) -> IoError,
+>;
+type ActorToProto<S> = future::Map<
+    Forward<ZipReceiverPeer, ProtoToHpv<S>>,
+    fn((ZipReceiverPeer, ProtoToHpv<S>)) -> (),
+>;
+type ActorSink = SinkMapErr<mpsc::Sender<HpvMsg>, fn(mpsc::SendError<HpvMsg>) -> IoError>;
+type AndThenSomething<S> = stream::AndThen<
+    Zip<ProtoSource<S>, stream::Repeat<Peer, IoError>>,
+    fn((BytesMut, Peer)) -> Result<HpvMsg, IoError>,
+    Result<HpvMsg, IoError>,
+>;
+type ProtoToActor<S> = future::Map<
+    Forward<AndThenSomething<S>, ActorSink>,
+    fn((AndThenSomething<S>, ActorSink)) -> (),
+>;
+type SelectActorProtoBridge<S> = future::Select<ProtoToActor<S>, ActorToProto<S>>;
+type ForgetfulBridge<S> = future::Map<
+    SelectActorProtoBridge<S>,
+    fn(((), future::SelectNext<ProtoToActor<S>, ActorToProto<S>>)) -> (),
+>;
+type NormalizedBridge<S> = future::MapErr<
+    ForgetfulBridge<S>,
+    fn(
+        (
+            IoError,
+            future::SelectNext<ProtoToActor<S>, ActorToProto<S>>,
+        ),
+    ) -> IoError,
+>;
+
+fn hyparview_protocol<S>(socket: S, config: HpvConfig) -> HpvFuture<S>
 where
     S: AsyncRead + AsyncWrite + Send + 'static,
 {
@@ -82,12 +143,13 @@ where
     // message, such that the message processor can communicate to the remote party
     let (send, recv): (mpsc::Sender<HpvMsg>, mpsc::Receiver<HpvMsg>) = mpsc::channel(10);
 
-    let (proto_writer, proto_reader) = Framed::new(socket, codec::UviBytes::<Vec<u8>>::default())
-        .from_err::<IoError>()
-        .split();
+    let (proto_writer, proto_reader): (ProtoSink<S>, ProtoSource<S>) =
+        Framed::new(socket, codec::UviBytes::<Vec<u8>>::default())
+            .from_err::<IoError>()
+            .split();
 
     // Transform outgoing HpvMsg messages to protobuf HpvMessage
-    let hpv_writer =
+    let hpv_writer: ProtoToHpv<S> =
         proto_writer.with::<_, fn((_, _)) -> _, _>(|(request, self_addr)| -> Result<_, IoError> {
             let proto_struct = msg_to_proto(request, self_addr);
             Ok(proto_struct.write_to_bytes().unwrap()) // TODO: error?
@@ -95,43 +157,74 @@ where
 
     // Get a copy of our local network address in protobuf
     // FIXME: derive Peer from mpsc::Sender
-    let mock_peer = stream::repeat(config.mock_temp_peer);
+    let mock_peer: stream::Repeat<Peer, IoError> = repeat(config.mock_temp_peer);
 
     // Transform incoming HpvMessage protobuf requests to HpvMsg, including the sender part for
     // this connection, such that the handler can talk back
-    let hpv_reader = proto_reader.zip(mock_peer).and_then(|(bytes, remote)| {
-        let response = protobuf::parse_from_bytes(&bytes)?;
-        proto_to_msg(response, remote)
-    });
+    let hpv_reader: HpvToProto<S> = proto_reader.zip(mock_peer).and_then::<fn((_, _)) -> _, _>(
+        |(bytes, remote)| {
+            let response = protobuf::parse_from_bytes(&bytes)?;
+            proto_to_msg(response, remote)
+        },
+    );
 
     // All messages received back from the HpvMsg processing actor using the channel of _this_
     // connection should be forwarded to the remote party
-    let my_addr_stream = stream::repeat(config.my_addr_proto);
-    let forward = recv
+    let my_addr_stream: stream::Repeat<proto::hpv::Peer, ()> = repeat(config.my_addr_proto);
+    let actor_to_proto: ActorToProto<S> = recv
         .zip(my_addr_stream)
-        .map_err(|e| IoError::new(IoErrorKind::UnexpectedEof, "Channel closed"))
-        .forward(hpv_writer);
+        .map_err::<_, fn(_) -> _>(|e| IoError::new(IoErrorKind::UnexpectedEof, "Channel closed"))
+        .forward(hpv_writer)
+        .map(forget);
 
     // Get a reference to our singleton actor that should receive messages of the HyParView protocol
-    let self_peer = config.self_peer.clone();
+    let self_peer: ActorSink = config
+        .self_peer
+        .clone()
+        .sink_map_err::<fn(_) -> _, _>(|e| IoError::new(IoErrorKind::ConnectionAborted, "Boom"));
     // Send all messages received from the remote side to the message processor
-    //    let source = self_peer.send_all(hpv_reader);
-    let source = hpv_reader
-        .forward(self_peer.sink_map_err(|e| IoError::new(IoErrorKind::ConnectionAborted, "Boom")));
+    //    let source = hpv_reader.forward(
+    //        RecipientWrapper(self_peer).sink_map_err::<fn(_) -> _, _>(|e| {
+    //            IoError::new(IoErrorKind::ConnectionAborted, "Boom")
+    //        }),
+    //    );
+    let proto_to_actor: ProtoToActor<S> = hpv_reader.forward(self_peer).map(forget);
 
     // Construct a Future by aligning sink and source and selecting either
-    let final_sink = forward.map(|_| ());
-    let final_source = source.map(|_| ());
-    let final_future = to_impl(
-        final_source
-            .select(final_sink)
-            .map(|_| ())
-            // TODO: Try holding on to error cause
-            .map_err(|e| IoError::new(IoErrorKind::UnexpectedEof, "Channel closed")),
-    );
+    let bridge: SelectActorProtoBridge<S> = proto_to_actor.select(actor_to_proto);
+    let forgetful_bridge: ForgetfulBridge<S> = bridge.map(forget);
+    let normalized: NormalizedBridge<S> = forgetful_bridge.map_err(unexpected_eof);
 
-    HpvFuture {
-        inner: Box::new(final_future) as Box<_>,
+    HpvFuture { inner: normalized }
+}
+
+fn forget<T>(_: T) {}
+
+fn unexpected_eof<T>(_: T) -> IoError {
+    IoError::new(IoErrorKind::UnexpectedEof, "Channel closed")
+}
+
+struct RecipientWrapper(dev::Recipient<HpvMsg>);
+
+impl Sink for RecipientWrapper {
+    type SinkItem = HpvMsg;
+    type SinkError = dev::SendError<HpvMsg>;
+
+    fn start_send(&mut self, item: HpvMsg) -> Result<AsyncSink<HpvMsg>, dev::SendError<HpvMsg>> {
+        unimplemented!()
+        //        match self.0.do_send(item) {
+        //            Ok(()) => Ok(AsyncSink::Ready),
+        //            Err(dev::SendError::Full(item)) => Ok(AsyncSink::NotReady(item)),
+        //            Err(e @ dev::SendError::Closed(_)) => Result::from_error(e),
+        //        }
+    }
+
+    fn poll_complete(&mut self) -> Result<Async<()>, dev::SendError<HpvMsg>> {
+        unimplemented!()
+    }
+
+    fn close(&mut self) -> Result<Async<()>, dev::SendError<HpvMsg>> {
+        unimplemented!()
     }
 }
 
